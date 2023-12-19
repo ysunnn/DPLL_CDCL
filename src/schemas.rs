a348dcc7b286c48c94c1a5b1a6004981ae814711
Lukas Mahr <lukas@yousuckatprogramming.de>
2023-12-19 01:01:20 +0100

    Update Formular to have a assigment stack to backtrack the assigments on collistion

--- src/schemas.rs
+++ src/schemas.rs
@@ -1,18 +1,21 @@
 use std::collections::VecDeque;
+
 #[derive(PartialEq, Debug)]
 pub enum Value {
     Null,
     True,
     False,
 }
+
 /// The clause struct
 ///
 /// Contains the list of [`literals`](Vec<i32>), the number of [`active_literals`](i32) and the [`satisfiable`](bool) flag.
 /// The [`satisfiable`](bool) flag is used to determine if the clause is satisfied.
 pub struct Clause {
     pub(crate) satisfiable: bool,
+    pub(crate) satisfied_by_variable: i32,
     pub(crate) literals: Vec<i32>,
-    pub(crate) active_literals: i32,
+    pub(crate) number_of_active_literals: i32,
 }
 
 /// The variable struct
@@ -25,6 +28,15 @@ pub struct Variable {
     pub(crate) negative_occurrences: Vec<i32>,
 }
 
+/// The assignment struct
+///
+/// Contains the variable and the value that was assigned to it.
+/// This struct is used to store the assignments in the [`assigment_stack`](Vec<Assignment>).
+pub struct Assignment {
+    pub(crate) variable: usize,
+    pub(crate) value: Value,
+}
+
 /// The formula struct
 ///
 /// Combines the list of [`clauses`](Clause) and the list of [`variables`](Variable).
@@ -33,4 +45,5 @@ pub struct Formula {
     pub(crate) clauses: Vec<Clause>,
     pub(crate) variables: Vec<Variable>,
     pub(crate) units: VecDeque<usize>,
+    pub(crate) assigment_stack: Vec<Assignment>,
 }
