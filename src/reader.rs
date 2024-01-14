006f4c4769f4bf1eade13168cd1210841756f9c7
You Sun Song <y.song@campus.lmu.de>
2024-01-15 00:49:21 +0100

    make dpll print output lines in DIMACS format
    Simplify reader.rs

--- src/reader.rs
+++ src/reader.rs
@@ -1,13 +1,13 @@
 use std::collections::VecDeque;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::str::FromStr;
+use std::io::{Read};
+use std::path::PathBuf;
 use crate::schemas::{Clause, Formula, Value, Variable};
 
 
 impl Variable {
     /// Create new variable
-    fn create_variable(_num: u16) -> Self {
+    fn new() -> Self {
         Self {
             value: Value::Null,
             positive_occurrences: Vec::new(),
@@ -19,18 +19,14 @@ impl Variable {
 
 impl Clause {
     /// Read line and convert it to Clause
-    fn create_clause(s: &str, variables: &mut Vec<Variable>, clause_index: usize) -> Result<Self, String> {
+    fn create_clause(s: &str, variables: &mut Vec<Variable>, clause_index: usize) -> Self {
         // Split as part by whitespace
-        let parts: Vec<&str> = s.split_whitespace().filter(|p| !p.is_empty()).collect();
-        // Check if the last part is zero
-        if parts.last() != Some(&"0") {
-            return Err("Invalid format: last part of clause is not 0".to_string());
-        }
+        let parts: Vec<&str> = s.split_whitespace().collect();
         let mut literals: Vec<i16> = Vec::new();
-        for part in &parts[..parts.len() - 1] { // except the last past(zero)
-            let lit = i16::from_str(part).map_err(|e| e.to_string())?;
+        for part in &parts[..parts.len()] {
+            let lit = part.parse::<i16>().expect("Can parse number");
             if lit == 0 {
-                return Err("Invalid format: Zero in clause".to_string());
+                continue;
             }
             literals.push(lit);
             let var = lit.abs() as usize;
@@ -42,31 +38,31 @@ impl Clause {
                 variables[var - 1].negative_occurrences.push(clause_index);
             }
         }
-        Ok(Self {
+        Self {
             satisfiable: false,
             satisfied_by_variable: 0,
             number_of_active_literals: literals.len() as u8,
             literals,
 
-        })
+        }
     }
 }
 
 
 impl Formula {
     /// Read a DIMACS CNF file and convert it to predefined Formula
-    pub fn read_formula(filename: &str) -> Result<Self, String> {
-        let file = File::open(filename).map_err(|e| e.to_string())?;
-        let reader = BufReader::new(file);
+    pub fn from_file(filename: &PathBuf) -> Result<Self, &str> {
+        let mut file = File::open(filename).expect("File not found");
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).expect("Error reading file");
 
         let mut clause_index = 0;
         let mut clauses = Vec::new();
         let mut variables = Vec::new();
-        let mut units = VecDeque::new();
-        let mut assigment_stack = Vec::new();
+        let units = VecDeque::new();
+        let assigment_stack = Vec::new();
 
-        for line in reader.lines() {
-            let line = line.map_err(|e| e.to_string())?;
+        for line in contents.lines() {
             let line = line.trim();
             // Ignore empty lines and comments
             if line.is_empty() || line.starts_with('c') {
@@ -76,25 +72,21 @@ impl Formula {
             if line.starts_with('p') {
                 let parts: Vec<&str> = line.split_whitespace().collect();
                 if parts.len() != 4 || parts[1] != "cnf" {
-                    return Err("Invalid header".to_string());
+                    return Err("Invalid header");
                 }
-                let num_vars = u16::from_str(parts[2]).map_err(|e| e.to_string())?;
-                let num_clauses = u16::from_str(parts[3]).map_err(|e| e.to_string())?;
-                clauses.reserve(num_clauses as usize);
-                variables.reserve(num_vars as usize);
-                for i in 1..=num_vars {
-                    let var = Variable::create_variable(i);
-                    variables.push(var);
-                }
+                let num_vars = parts[2].parse::<u16>().expect("Can parse num vars");
+                let num_clauses = parts[3].parse::<u16>().expect("Can parse num clauses");
+                clauses = Vec::with_capacity(num_clauses as usize);
+                variables = vec![Variable::new(); num_vars as usize];
             } else {
-                let clause = Clause::create_clause(line, &mut variables, clause_index)?;
+                let clause = Clause::create_clause(line, &mut variables, clause_index);
                 clauses.push(clause);
                 clause_index += 1;
             }
         }
 
         if clauses.is_empty() || variables.is_empty() {
-            return Err("file is empty".to_string());
+            return Err("file is empty");
         }
 
         Ok(Self {
